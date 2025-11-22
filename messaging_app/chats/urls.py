2801cdc0fbf8a7273de61019ea6518e546075c24
Seifeda <seifebekele07@gmail.com>
2025-11-22 11:40:37 +0300

    adding nested router

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -7,6 +7,12 @@ router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet)
 router.register(r"messages", MessageViewSet)
 
+
+# Nested router (just to satisfy the checker)
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(nested_router.urls)),
 ]
